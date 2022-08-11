ae8e674f8cdc0c7818cd0e10c27641251fc2d077
sserges <agounkeserge@gmail.com>
2022-08-11 17:48:48 -0400

    updatePost method added to PostController

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers'
 
 import App from './App'
+import './index.css'
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import mongoose from 'mongoose'
 
 import PostMessage from '../models/postMessage.js'
 
@@ -13,4 +14,19 @@ export const createPost = asyncHandler(async (req, res) => {
   const newPost = await new PostMessage(post).save()
 
   res.status(201).json(newPost)
+})
+
+export const updatePost = asyncHandler(async (req, res) => {
+  const { id: _id } = req.params
+  const post = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('No post with that id')
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  })
+
+  res.json(updatePost)
 })
